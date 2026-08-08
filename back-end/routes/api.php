7c12da0e2c3605d435e0847f0267041bc1dd06e8
Michel Mardochée <mickmard94@gmail.com>
2026-08-08 14:03:36 -0400

    Ajout des routes admin manquantes

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\SolController;
 use App\Http\Controllers\Api\MembreController;
 use App\Http\Controllers\Api\CotisationController;
 use App\Http\Controllers\Api\FeedbackController;
+use App\Http\Controllers\Api\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('throttle:6,1')->group(function () {
@@ -12,7 +13,6 @@ Route::middleware('throttle:6,1')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 });
 
-// Accessible avec ou sans connexion, mais limité pour éviter le spam.
 Route::middleware('throttle:10,1')->group(function () {
     Route::post('feedback', [FeedbackController::class, 'store']);
 });
@@ -37,4 +37,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('cotisations', [CotisationController::class, 'store']);
     Route::delete('cotisations/{cotisation}', [CotisationController::class, 'destroy']);
+
+    Route::post('admin/promouvoir', [AdminController::class, 'promouvoir']);
+    Route::get('admin/utilisateurs', [AdminController::class, 'utilisateurs']);
+    Route::get('admin/avis', [AdminController::class, 'avis']);
 });
