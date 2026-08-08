dc7fa503ca98bfe69cda109558cc05743c541f74
Michel Mardochée <mickmard94@gmail.com>
2026-08-08 14:28:51 -0400

    Correction URL CORS (vraie URL onrender actuelle)

--- back-end/config/cors.php
+++ back-end/config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://sol-ansanm.onrender.com',
+        'https://soltrack-frontend.onrender.com',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
